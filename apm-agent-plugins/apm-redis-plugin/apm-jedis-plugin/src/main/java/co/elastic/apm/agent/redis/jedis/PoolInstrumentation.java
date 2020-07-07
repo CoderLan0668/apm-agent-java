457917d9b30c5434528ea3ec1f83efc89d86df58
lan <lanjian_0668@163.com>
2020-07-07 11:21:33 +0800

    rename jedis getResource span name to constant simple value

--- apm-agent-plugins/apm-redis-plugin/apm-jedis-plugin/src/main/java/co/elastic/apm/agent/redis/jedis/PoolInstrumentation.java
+++ apm-agent-plugins/apm-redis-plugin/apm-jedis-plugin/src/main/java/co/elastic/apm/agent/redis/jedis/PoolInstrumentation.java
@@ -11,9 +11,9 @@
  * the Apache License, Version 2.0 (the "License"); you may
  * not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -59,19 +59,14 @@ public class PoolInstrumentation extends ElasticApmInstrumentation {
     }
 
     @Advice.OnMethodEnter(suppress = Throwable.class)
-    public static void onEnter(@Nullable @Advice.Local("span") Span span,
-                               @Advice.This Object that) {
+    public static void onEnter(@Nullable @Advice.Local("span") Span span) {
         if (tracer == null || tracer.getActive() == null) {
             return;
         }
 
         AbstractSpan<?> parent = tracer.getActive();
-        String className = that.getClass().getName();
-        String simpleClassName = className.substring(className.lastIndexOf('$') + 1);
-        simpleClassName = simpleClassName.substring(simpleClassName.lastIndexOf('.') + 1);
         span = parent.createSpan()
-            .withName(simpleClassName)
-            .withName("#getResource")
+            .withName("redis#getResource")
             .activate();
     }
 
